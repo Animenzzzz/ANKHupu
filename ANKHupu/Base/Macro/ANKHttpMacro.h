bee9542727e8025857fdc782b16ea76bf17f4086
Animenzzz <kangjian_037@126.com>
2019-05-15 20:40:30 +0800

    1.修改一些域名宏

--- ANKHupu/Base/Macro/ANKHttpMacro.h
+++ ANKHupu/Base/Macro/ANKHttpMacro.h
@@ -12,15 +12,48 @@
 //虎扑域名
 #define kHPServiceHost @"https://games.mobileapi.hupu.com"
 
-//NBA address+params
-#define kNBA           @"/3/7.3.12/nba/getNews"
-#define kNBAParams     @"?news_first_navi=NBA&pre_count=0&advId=E12875A5-1076-4C57-9488-B5311B604032&clientId=30980511&nid=0&first_navi_numbers=2&preload=0&direc=next&num=20&_ssid=VFQtUXVXYW4xN0Y&night=0&crt=1557906702&time_zone=Asia%2FShanghai&client=c77bc7cfa00b1800f399938c4b3720aae4783b2a&sign=53d4ad9a87f32e057cb986611e72e9c1"
-#define kNBA_FullPath [NSString stringWithFormat:@"%@%@%@",kHPServiceHost,kNBA,kNBAParams]
+//以下为固定参数 8个
+#define kParam_client      @"client=c77bc7cfa00b1800f399938c4b3720aae4783b2a"
+#define kParam_clientId    @"clientId=30980511"
+#define kParam_advId       @"advId=E12875A5-1076-4C57-9488-B5311B604032"
+#define kParam_ssid        @"_ssid=VFQtUXVXYW4xN0Y"
+#define kParam_night       @"night=0"
+#define kParam_crt         @"crt=1557906702"
+#define kParam_time_zone   @"time_zone=Asia%2FShanghai"
+#define kParam_sign        @"sign=53d4ad9a87f32e057cb986611e72e9c1"
+//固定参数拼接
+#define kParam_full  [NSString stringWithFormat:@"%@?%@?%@?%@?%@?%@%@?%@",kParam_client,kParam_clientId,kParam_advId,kParam_ssid,kParam_night,kParam_crt,kParam_time_zone,kParam_sign]
 
-//热搜 address+params
+
+
+//------NBA（额外参数：7个）
+#define kNBA           @"/3/7.3.12/nba/getNews"
+#define kNBA_param1    @"news_first_navi=NBA"
+#define kNBA_param2    @"pre_count=0"
+#define kNBA_param3    @"nid=0"
+#define kNBA_param4    @"first_navi_numbers=2"
+#define kNBA_param5    @"preload=0"
+#define kNBA_param6    @"direc=next"
+#define kNBA_param7    @"num=20"
+#define kNBA_full_Params    [NSString stringWithFormat:@"?%@?%@?%@?%@?%@?%@%@?%@",kNBA_param1,kNBA_param2,kNBA_param3,kNBA_param4,kNBA_param5,kNBA_param6,kNBA_param7,kParam_full]
+#define kNBA_FullPath [NSString stringWithFormat:@"%@%@%@",kHPServiceHost,kNBA,kNBA_full_Params]
+
+
+//------热搜（额外参数：0个）
 #define kHotSearch          @"/3/7.3.12/search/hotkeylist"
-#define kHotSearchParams     @"?time_zone=Asia%2FShanghai&client=c77bc7cfa00b1800f399938c4b3720aae4783b2a&advId=E12875A5-1076-4C57-9488-B5311B604032&night=0&crt=1557906417&clientId=30980511&_ssid=VFQtUXVXYW4xN0Y&sign=95fab141fc4cb5ebf008ed7427b6de84"
+#define kHotSearchParams    [NSString stringWithFormat:@"?%@",kParam_full]
 #define kHotSearch_FullPath [NSString stringWithFormat:@"%@%@%@",kHPServiceHost,kHotSearch,kHotSearchParams]
+
+
+//------推荐（额外参数：1个）
+#define kHotList            @"/3/7.3.12/buffer/hotList"
+#define kHotList_param1     @"is_first=0"
+#define kHotList_full_Params [NSString stringWithFormat:@"?%@?%@",kHotList_param1,kParam_full]
+#define kHotList_FullPath [NSString stringWithFormat:@"%@%@%@",kHPServiceHost,kHotList,kHotList_full_Params]
+
+
+
+
 
 
 #endif /* ANKHttpMacro_h */
