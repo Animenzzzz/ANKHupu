fa8391fe4fe23a117e304830dc7cb5db7effe929
Animenzzz <kangjian_037@126.com>
2019-10-25 16:04:52 +0800

    --新增：FBShimmering(Facebook文字闪烁框架)、新闻详情骨架屏

---
+++ ANKHupu/News/Other/H5Detail/View/H5DetailSkeletonCell.h
@@ -0,0 +1,17 @@
+//
+//  H5DetailSkeletonCell.h
+//  ANKHupu
+//
+//  Created by Animenzzz on 2019/10/25.
+//  Copyright © 2019 Animenzzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface H5DetailSkeletonCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ANKHupu/News/Other/H5Detail/View/H5DetailSkeletonCell.m
@@ -0,0 +1,24 @@
+//
+//  H5DetailSkeletonCell.m
+//  ANKHupu
+//
+//  Created by Animenzzz on 2019/10/25.
+//  Copyright © 2019 Animenzzz. All rights reserved.
+//
+
+#import "H5DetailSkeletonCell.h"
+
+@implementation H5DetailSkeletonCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
